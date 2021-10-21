715d3e04a3b2031e2e2917046b9dc3101a7de687
Scott Nicol <s.cott@me.com>
2021-10-21 14:45:46 +0100

    initial commit

---
+++ README.md
@@ -0,0 +1,9 @@
+# paw-secretbox-decrypt
+
+Extension to open NaCl secretboxes (XSalsa20 + Poly1305) in Paw.
+
+    git clone git@github.com:snicol/paw-secretbox-decrypt.git
+
+    mkdir -p ~/Library/Containers/com.luckymarmot.Paw/Data/Library/Application\ Support/com.luckymarmot.Paw/Extensions/com.snicol.SecretboxDecrypt
+
+    cp -r paw-secretbox-decrypt/ '~/Library/Containers/com.luckymarmot.Paw/Data/Library/Application\ Support/com.luckymarmot.Paw/Extensions/com.snicol.SecretboxDecrypt'
---
+++ src/SecretboxDecrypt.js
@@ -0,0 +1,40 @@
+// var Buffer = require('buffer/').Buffer
+
+const nacl = require('tweetnacl');
+const naclutil = require('tweetnacl-util');
+
+class SecretboxDecrypt {
+	evaluate() {
+		if([this.message, this.key, this.nonce].some(v => v === null))
+			return '';
+
+		const res = nacl.secretbox.open(
+			naclutil.decodeBase64(this.message),
+			naclutil.decodeBase64(this.nonce),
+			naclutil.decodeBase64(this.key)
+		);
+
+		if (res === null)
+			return 'authenticating message failed';
+
+		return naclutil.encodeUTF8(res);
+	}
+}
+
+SecretboxDecrypt.identifier = 'com.snicol.SecretboxDecrypt';
+SecretboxDecrypt.title = 'Secretbox Decrypt';
+SecretboxDecrypt.help = 'https://github.com/snicol/paw-secretbox-decrypt';
+
+SecretboxDecrypt.inputs = [
+	InputField('message', 'Message', 'SecureValue', {
+		persisted: true,
+	}),
+	InputField('key', 'Key', 'SecureValue', {
+		persisted: true,
+	}),
+	InputField('nonce', 'Nonce', 'SecureValue', {
+		persisted: true,
+	}),
+];
+
+registerDynamicValueClass(SecretboxDecrypt);
